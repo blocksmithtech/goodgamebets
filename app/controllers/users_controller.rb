153e7f1dd2b08120b9d6e13857a1f25813c432db
Gonçalo Valério <gon@ovalerio.net>
2018-04-30 17:12:41 +0100

    added a few changes to the landing page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,11 @@ class UsersController < ApplicationController
     unless @user == current_user
       redirect_to root_path, :alert => "Access denied."
     end
+    kit_users = OSTKitService.new.users.list.data['economy_users']
+    kit_user = kit_users.select do |item|
+      item['uuid'] == @user.wallet
+    end
+    @amount = kit_user[0]['token_balance']
   end
 
 end
