ae2175b7d102fb385c57be741c82c4f9e2a84b8c
Gonçalo Valério <gon@ovalerio.net>
2018-05-08 15:12:30 +0100

    added the create bet base functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
 
+  resources :bets, only: [:create]
+
   # config/routes.rb
   authenticate :admin_user do
     mount Sidekiq::Web => '/sidekiq'
