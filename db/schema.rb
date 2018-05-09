aa4f7259d792f5613c0c774c56876231a8668941
Gonçalo Valério <gon@ovalerio.net>
2018-05-09 16:04:44 +0100

    added score fields and marked the winner field as optional

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_27_141751) do
+ActiveRecord::Schema.define(version: 2018_05_09_145444) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,9 @@ ActiveRecord::Schema.define(version: 2018_04_27_141751) do
     t.integer "winner_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "home_score", default: 0
+    t.integer "away_score", default: 0
+    t.integer "state", default: 0
   end
 
   create_table "teams", force: :cascade do |t|
