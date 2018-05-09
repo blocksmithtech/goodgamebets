3fa396cc2a492b65c6be62605b80539e529abd88
Gonçalo Valério <gon@ovalerio.net>
2018-05-09 16:04:44 +0100

    added score fields and marked the winner field as optional

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,10 @@
 class Game < ApplicationRecord
   belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id'
   belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
-  belongs_to :winner, class_name: 'Team', foreign_key: 'winner_id'
+  belongs_to :winner, class_name: 'Team', foreign_key: 'winner_id', optional: true
 
   has_many :bets
   has_many :users, through: :bets
+
+  enum state: { created: 0, archived: 1, completed: 2 }
 end
